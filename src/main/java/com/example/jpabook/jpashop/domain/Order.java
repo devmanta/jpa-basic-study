4a447769660e6d8fc350ecbe5fd65baa0797caee
heejin <dlwlrgkwk82@gmail.com>
2023-09-09 16:07:29 +0900

    feat: add order create, cancel, getPrice

--- src/main/java/com/example/jpabook/jpashop/domain/Order.java
+++ src/main/java/com/example/jpabook/jpashop/domain/Order.java
@@ -1,10 +1,12 @@
 package com.example.jpabook.jpashop.domain;
 
 import com.example.jpabook.global.BaseEntity;
+import com.example.jpabook.jpashop.domain.enumtype.DeliveryStatus;
 import com.example.jpabook.jpashop.domain.enumtype.OrderStatus;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -15,7 +17,8 @@ import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 @Table(name = "orders")
-@Getter @Setter
+@Getter
+@RequiredArgsConstructor
 public class Order extends BaseEntity {
 
     @ManyToOne(fetch = LAZY)
@@ -42,6 +45,41 @@ public class Order extends BaseEntity {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    @Builder
+    public Order(Member member, List<OrderItem> orderItems, Delivery delivery, LocalDateTime orderDate, OrderStatus status) {
+        this.member = member;
+        this.orderItems = orderItems;
+        this.delivery = delivery;
+        this.orderDate = orderDate;
+        this.status = status;
+    }
+
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        return Order.builder()
+                .member(member)
+                .delivery(delivery)
+                .orderItems(List.of(orderItems))
+                .status(OrderStatus.ORDER)
+                .orderDate(LocalDateTime.now())
+                .build();
+    }
+
+    public void cancel() {
+        if (this.delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("cancellation is rejected due to delivery is completed.");
+        }
+
+        this.status = OrderStatus.CANCEL;
+
+        for (OrderItem orderItem : this.orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    public int getTotalPrice() {
+        return this.orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
     }
 
 }
--- src/main/java/com/example/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/com/example/jpabook/jpashop/domain/OrderItem.java
@@ -3,16 +3,13 @@ package com.example.jpabook.jpashop.domain;
 import com.example.jpabook.global.BaseEntity;
 import com.example.jpabook.jpashop.domain.item.Item;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Getter @Setter
+@Getter
 public class OrderItem extends BaseEntity {
 
     @ManyToOne(fetch = LAZY)
@@ -25,5 +22,36 @@ public class OrderItem extends BaseEntity {
 
     private int orderPrice;
     private int count;
+
+    @Builder
+    public OrderItem(Item item, Order order, int orderPrice, int count) {
+        this.item = item;
+        this.order = order;
+        this.orderPrice = orderPrice;
+        this.count = count;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        item.removeStock(count);
+
+        return OrderItem.builder()
+                .item(item)
+                .orderPrice(orderPrice)
+                .count(count)
+                .build();
+    }
+
+    public void cancel() {
+        this.item.addStock(this.count);
+    }
+
+    public int getTotalPrice() {
+        // order price might be diff from item price (discount etc)
+        return this.orderPrice * this.count;
+    }
 
 }
